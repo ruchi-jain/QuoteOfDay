74d16013238864cfedd8a1721b9ecf6bc6e3eb89
Ruchi Goel <ruchi.goel@outlook.com>
2016-05-30 16:42:44 -0700

    added twitter and tumbler code

--- js/main.js
+++ js/main.js
@@ -1,16 +1,31 @@
 var myColors = ['#FDE3A7', '#F1A9A0', '#AEA8D3', '#A2DED0', '#89C4F4'];
+var currentQuote = '', currentAuthor = '';
 
 $(document).ready(function () {
     getQuote();
-    $("button").click(function () {
+    $("#new-quote").click(function () {
         var randomize = Math.floor(Math.random() * myColors.length);
+        console.log(randomize);
         $('body').css("background-color", myColors[randomize]);
+        console.log("Call getQuote");
         getQuote();
         
     });
+    
+    $('#tweet-quote').on('click', function() {  
+      openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
+    
+  });
+  $('#tumblr-quote').on('click', function() {
+ 
+      openURL('https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes&freecodecamp&caption='+encodeURIComponent(currentAuthor)+'&content=' + encodeURIComponent(currentQuote));
+    
+  });
 });
 
+
 function getQuote(){
+    console.log("in getQuote");
     $.ajax({
             method: "POST",
             url: "https://andruxnet-random-famous-quotes.p.mashape.com/cat=famous",
@@ -20,8 +35,12 @@ function getQuote(){
             },
             success: function (data) {
                 var val = JSON.parse(data);
+                currentQuote= val["quote"];
+                currentAuthor= val["author"];
                 console.log(val);
-                $("#div1").html(val["quote"] + "<br> <br> <span> - " + val["author"] + "</span>");
+                $(".quote-text").html(currentQuote + "<br> <br> <span class='quote-author'> - " + currentAuthor + "</span>");
+                $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
+        $('#tumblr-quote').attr('href', 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes&freecodecamp&caption='+encodeURIComponent(currentAuthor)+'&content=' + encodeURIComponent(currentQuote));
             }
         });
 }
